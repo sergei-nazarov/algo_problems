f965f1897ddb7780de2e2e26d6400cab5016cfc9
sergey <mamban1997@gmail.com>
2023-02-26 19:34:27 +0300

    send more money 2 genetic algorithm

--- src/genetic_algorithm/Chromosome.java
+++ src/genetic_algorithm/Chromosome.java
@@ -5,7 +5,8 @@ import java.util.List;
 public abstract class Chromosome<T extends Chromosome<T>> implements Comparable<T> {
     public abstract double fitness();
 
-    public abstract List<T> crossover();
+    public abstract List<T> crossover(T other);
+    public abstract void mutate();
 
     public abstract T copy();
 
--- src/genetic_algorithm/GeneticAlgorithm.java
+++ src/genetic_algorithm/GeneticAlgorithm.java
@@ -1,4 +1,101 @@
 package genetic_algorithm;
 
-public class GeneticAlgorithm {
+import java.util.*;
+
+public class GeneticAlgorithm<C extends Chromosome<C>> {
+
+    public enum SelectionType {
+        ROULETTE, TOURNAMENT
+    }
+
+    private List<C> population;
+    private double mutationChance;
+    private double crossoverChance;
+    private SelectionType selectionType;
+    private Random random;
+
+    public GeneticAlgorithm(List<C> initialPopulation, double mutationChance, double crossoverChance, SelectionType selectionType) {
+        this.population = initialPopulation;
+        this.mutationChance = mutationChance;
+        this.crossoverChance = crossoverChance;
+        this.selectionType = selectionType;
+        random = new Random();
+    }
+
+    private List<C> pickRoulette(double[] wheel, int numPicks) {
+        List<C> picks = new ArrayList<>();
+        for (int i = 0; i < numPicks; i++) {
+            double pick = random.nextDouble();
+            for (int j = 0; j < wheel.length; j++) {
+                pick -= wheel[j];
+                if (pick <= 0) {
+                    picks.add(population.get(j));
+                    break;
+                }
+            }
+        }
+        return picks;
+    }
+
+    private List<C> pickTournament(int numParticipants, int numPicks) {
+        Collections.shuffle(population, random);
+        List<C> tournament = population.subList(0, numParticipants);
+        tournament.sort(Collections.reverseOrder());
+        return tournament.subList(0, numPicks);
+    }
+
+    private void reproduceAndReplace() {
+        List<C> nextPopulation = new ArrayList<>();
+        while (nextPopulation.size() < population.size()) {
+            List<C> parents = new ArrayList<>();
+            if (selectionType == SelectionType.ROULETTE) {
+                double totalFitness = population.stream().mapToDouble(Chromosome::fitness).sum();
+                double[] wheel = population.stream().mapToDouble(x -> x.fitness() / totalFitness).toArray();
+                parents = pickRoulette(wheel, 2);
+            } else {
+                parents = pickTournament(population.size() / 2, 2);
+            }
+
+            if (random.nextDouble() < crossoverChance) {
+                C parent1 = parents.get(0);
+                C parent2 = parents.get(1);
+                nextPopulation.addAll(parent1.crossover(parent2));
+            } else {
+                nextPopulation.addAll(parents);
+            }
+
+        }
+        if (nextPopulation.size() > population.size()) {
+            nextPopulation.remove(0);
+        }
+        population = nextPopulation;
+    }
+
+    private void mutate() {
+        for (C individual : population) {
+            if (random.nextDouble() < mutationChance) {
+                individual.mutate();
+            }
+        }
+    }
+
+    public C run(int maxGeneration, double threshold) {
+        C best = Collections.max(population).copy();
+        for (int generation = 0; generation < maxGeneration; generation++) {
+            if (best.fitness() >= threshold) {
+                return best;
+            }
+            System.out.println("Generation " + generation +
+                    " Best " + best.fitness() +
+                    " Avg " + population.stream().mapToDouble(Chromosome::fitness).average().orElse(0.0));
+            reproduceAndReplace();
+            mutate();
+            C highest = Collections.max(population);
+            if (highest.fitness() > best.fitness()) {
+                best = highest.copy();
+            }
+        }
+        return best;
+    }
 }
+
