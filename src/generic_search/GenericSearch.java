19f0a365df815c32abae48980dea6c70f8af1ab6
sergey <mamban1997@gmail.com>
2024-01-15 13:32:38 +0400

    test

--- src/generic_search/GenericSearch.java
+++ src/generic_search/GenericSearch.java
@@ -55,7 +55,7 @@ public class GenericSearch {
                 }
                 explored.add(child);
                 frontier.push(new Node<>(child, currentNode));
-            }////тест4 тест 5
+            }////тест4 тест 5 тест 6
         }
         System.out.println("Количество итераций: " + counter);
         return null;
